ed7860fd111922fc8aa6a57e2e9139f6de27e6a0
90042302 <109151576+90042302@users.noreply.github.com>
2022-11-14 21:16:55 +0900

    20221114

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,14 +1,10 @@
 import Video from "../models/Video";
 
-
-  // Video.find({},(error, videos) => {});   
-
   export const home = async(req, res) => {
     const videos = await Video.find({});
     //console.log(videos);
     return res.render("home", { pageTitle: "Home", videos});
     }; 
-
 
 
   export const watch = async(req, res) => {
@@ -20,7 +16,6 @@ import Video from "../models/Video";
     return res.render("watch", { pageTitle: video.title, video:video});
   };
 
-
   export const getEdit = async(req, res) => {
     const { id } = req.params;
     const video = await Video.findById(id);
@@ -29,21 +24,35 @@ import Video from "../models/Video";
       }
       return res.render("edit", { pageTitle: `Edit⚙ ${video.title}`, video});
     };
-  
-    
 
 
 
 
-  export const postEdit = (req, res) => {
+  export const postEdit = async (req, res) => {
     const { id } = req.params;
-    const { title } = req.body;
-    return res.redirect(`/videos/${id}`);
+    const { title, description, hashtags } = req.body;
+    const video = await Video.exists({ _id:id });
+    if(!video){
+      return res.render("404",{pageTitle:"Video not found"});
+    }
+    await Video.findByIdAndUpdate(id, {
+        title,
+        description,
+        hashtags : Video.formatHashtags(hashtags),
+      })
+      return res.redirect(`/videos/${id}`);
+    };
+   
+
+
+
+
+  export const getUpload = (req, res) => {
+  return res.render("upload",{ pageTitle: "Upload Video"});
   };
 
-export const getUpload = (req, res) => {
-    return res.render("upload",{ pageTitle: "Upload Video"});
-};
+    
+
 
 
 export const postUpload = async (req, res) => {
@@ -52,38 +61,17 @@ export const postUpload = async (req, res) => {
     await Video.create({
     title,
     description,
-    //createdAt: Date.now(),
-    hashtags : hashtags.split(",").map((word) => `#${word}`),
+    hashtags : Video.formatHashtags(hashtags),
   });
-  return res.redirect("/");
-}   
-//video 생성에 문제가 없으면 home으로 
-catch (error)
-  {
-    return res.render("upload",{ pageTitle: "Upload Video", 
-    errorMessage:error._message,});
+    return res.redirect("/");
+  } catch (error) {
+    return res.render("upload",{ 
+    pageTitle: "Upload Video", 
+    errorMessage:error._message,
+    });
   }
 };
-//오류가 있으면 upload를 다시 render
 
-/*
-export const postUpload = async (req, res) => {
-    const { title, description, hashtags } = req.body;
-    const video = new Video({
-      title,
-      description,
-      createdAt:Date.now(),
-      hashtags : hashtags.split(",").map((word) => `#${word}`),
-      meta : {
-        views : 0,
-        rating : 0,
-      },
-    });
-    await video.save();
-
-    return res.redirect("/");
-};
-*/
 
 
 
