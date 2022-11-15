4b753ce61860b14b152b81f8b75db572e6b07643
90042302 <109151576+90042302@users.noreply.github.com>
2022-11-15 23:13:15 +0900

    20221115

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,19 @@
-export const join = (req, res) => res.send("JOIN 🎈🎉✨🎈");
+import User from "../models/User";
 
+
+export const getJoin = (req, res) => res.render("join", {pageTitle : "join"});
+export const postJoin = async (req, res) => {
+    console.log(req.body);
+    const { name, username, email, password, location } = req.body;
+    await User.create({
+        name, 
+        username, 
+        email, 
+        password,
+        location,
+    });
+    return res.redirect("/login");
+}
 export const edit = (req, res) => res.send("Edit⛏🪓🔨🔧");
 
 export const remove = (req, res) => res.send("Remove User❌🔙🔙🔚🔚");
