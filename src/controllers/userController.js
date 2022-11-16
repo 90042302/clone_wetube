096da2550a79addf31884ad69e1f5d84e8d8d215
90042302 <109151576+90042302@users.noreply.github.com>
2022-11-16 23:09:12 +0900

    20221116

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -4,7 +4,18 @@ import User from "../models/User";
 export const getJoin = (req, res) => res.render("join", {pageTitle : "join"});
 export const postJoin = async (req, res) => {
     console.log(req.body);
-    const { name, username, email, password, location } = req.body;
+    const { name, username, email, password, password2, location } = req.body;
+    const pageTitle = "Join";
+    if(password !== password2){
+        return res.status(400).render("join", { pageTitle, 
+        errorMessage:"✔패스워드가 일치하지 않습니다"});
+    } //라턴을 해주지 않으면 코드가 계속됨!! (주의)
+    const exists = await User.exists({$or:[{username},{email}]});
+    if(exists){
+        return res.status(400).render("join", { pageTitle, 
+        errorMessage:"이미 사용중인 ✔username OR ✔email 입니다."});
+    }
+
     await User.create({
         name, 
         username, 
