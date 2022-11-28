abe4e9596d9a487f8d78666f9c6f36c266b23330
90042302 <109151576+90042302@users.noreply.github.com>
2022-11-28 23:00:03 +0900

    20221128

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,69 +1,136 @@
 import User from "../models/User";
+import fetch from "node-fetch";
 import bcrypt from "bcrypt";
-
-
-export const getJoin = (req, res) => res.render("join", {pageTitle : "join"});
+export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
 export const postJoin = async (req, res) => {
-    console.log(req.body);
-    const { name, username, email, password, password2, location } = req.body;
-    const pageTitle = "Join";
-    if(password !== password2){
-        return res.status(400).render("join", { pageTitle, 
-        errorMessage:"✔패스워드가 일치하지 않습니다"});
-    } //라턴을 해주지 않으면 코드가 계속됨!! (주의)
-    const exists = await User.exists({$or:[{username},{email}]});
-    if(exists){
-        return res.status(400).render("join", { pageTitle, 
-        errorMessage:"이미 사용중인 ✔username OR ✔email 입니다."});
-    }
-    try{
+  const { name, username, email, password, password2, location } = req.body;
+  const pageTitle = "Join";
+  if (password !== password2) {
+    return res.status(400).render("join", {
+      pageTitle,
+      errorMessage: "Password confirmation does not match.",
+    });
+  }
+  const exists = await User.exists({ $or: [{ username }, { email }] });
+  if (exists) {
+    return res.status(400).render("join", {
+      pageTitle,
+      errorMessage: "This username/email is already taken.",
+    });
+  }
+  try {
     await User.create({
-        name, 
-        username, 
-        email, 
-        password,
-        location,
+      name,
+      username,
+      email,
+      password,
+      location,
     });
     return res.redirect("/login");
-    } catch(error) {
-        return res.status(400).render("join",{ 
-            pageTitle: "join", 
-            errorMessage:error._message,
-        });
-    }
-}
-
-export const getLogin = (req, res) => res.render("login",{pageTitle:"Login"});
-
-
-export const postLogin = async(req, res) => {
-    const { username, password } = req.body;
-    const pageTitle = "Login";
-    const user = await User.findOne({ username });
-    if(!user){
-        return res
-        .status(400)
-        .render("login",
-        {pageTitle,
-        errorMessage:"입력한 username을 가진 User가 존재하지 않습니다"});
-    }
-    const ok = await bcrypt.compare(password, user.password);
-    if(!ok){
-
-        return res
-        .status(400)
-        .render("login",
-        {pageTitle,
-        errorMessage:"잘못된 password입니다."});
-    }
-    
-    req.session.loggedIn = true;
-    req.session.user = user;
-    return res.redirect("/");
+  } catch (error) {
+    return res.status(400).render("join", {
+      pageTitle: "Upload Video",
+      errorMessage: error._message,
+    });
+  }
 };
-
-
-export const edit = (req, res) => res.send("Edit⛏🪓🔨🔧");
-export const remove = (req, res) => res.send("Remove User❌🔙🔙🔚🔚");
-export const logout = (req, res) => res.send("LogOut🖐🏻");
-export const see = (req, res) => res.send("See User😎😋");
+export const getLogin = (req, res) =>
+  res.render("login", { pageTitle: "Login" });
+export const postLogin = async (req, res) => {
+  const { username, password } = req.body;
+  const pageTitle = "Login";
+  const user = await User.findOne({ username });
+  if (!user) {
+    return res.status(400).render("login", {
+      pageTitle,
+      errorMessage: "An account with this username does not exists.",
+    });
+  }
+  const ok = await bcrypt.compare(password, user.password);
+  if (!ok) {
+    return res.status(400).render("login", {
+      pageTitle,
+      errorMessage: "Wrong password",
+    });
+  }
+  req.session.loggedIn = true;
+  req.session.user = user;
+  return res.redirect("/");
+};
+export const startGithubLogin = (req, res) => {
+  const baseUrl = "https://github.com/login/oauth/authorize";
+  const config = {
+    client_id: process.env.GH_CLIENT,
+    allow_signup: false,
+    scope: "read:user user:email",
+  };
+  const params = new URLSearchParams(config).toString();
+  const finalUrl = `${baseUrl}?${params}`;
+  return res.redirect(finalUrl);
+};
+export const finishGithubLogin = async (req, res) => {
+  const baseUrl = "https://github.com/login/oauth/access_token";
+  const config = {
+    client_id: process.env.GH_CLIENT,
+    client_secret: process.env.GH_SECRET,
+    code: req.query.code,
+  };
+  const params = new URLSearchParams(config).toString();
+  const finalUrl = `${baseUrl}?${params}`;
+  const tokenRequest = await (
+    await fetch(finalUrl, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+      },
+    })
+  ).json();
+  if ("access_token" in tokenRequest) {
+    const { access_token } = tokenRequest;
+    const apiUrl = "https://api.github.com";
+    const userData = await (
+      await fetch(`${apiUrl}/user`, {
+        headers: {
+          Authorization: `token ${access_token}`,
+        },
+      })
+    ).json();
+    const emailData = await (
+      await fetch(`${apiUrl}/user/emails`, {
+        headers: {
+          Authorization: `token ${access_token}`,
+        },
+      })
+    ).json();
+    const emailObj = emailData.find(
+      (email) => email.primary === true && email.verified === true
+    );
+    if (!emailObj) {
+      return res.redirect("/login");
+    }
+    const existingUser = await User.findOne({ email: emailObj.email });
+    if (existingUser) {
+      req.session.loggedIn = true;
+      req.session.user = existingUser;
+      return res.redirect("/");
+    } else {
+      const user = await User.create({
+        name: userData.name,
+        username: userData.login,
+        email: emailObj.email,
+        password: "",
+        socialOnly: true,
+        location: userData.location,
+      });
+      req.session.loggedIn = true;
+      req.session.user = user;
+      return res.redirect("/");
+    }
+  } else {
+    return res.redirect("/login");
+  }
+};
+export const edit = (req, res) => res.send("Edit User");
+export const remove = (req, res) => res.send("Remove User");
+export const logout = (req, res) => res.send("Log out");
+export const see = (req, res) => res.send("See User");
--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 
+
 import express from "express";
 import morgan from "morgan";
 import session from "express-session";
+import MongoStore from "connect-mongo";
 import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
@@ -21,9 +23,10 @@ app.use(express.urlencoded({ extended: true }));
 //이제 이 미들웨어가 사이트로 들어오는 모두를 기억해줌
 app.use(
     session({
-        secret:"Hello!",
-        resave:true,
-        saveUninitialized:true,
+        secret:process.env.COOKIE_SECRET,
+        resave:false,
+        saveUninitialized:false,
+        store:MongoStore.create({ mongoUrl:process.env.DB_URL })
     })
 );
 
